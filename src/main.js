53a8678eadf13b11d5b8929bfa9bac58f6f46fbd
myl <yunliangmiao@gmail.com>
2017-10-14 00:45:35 +0800

    引入axios

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import axios from 'axios'
 // import router from './router'
 /* eslint-disable no-unused-vars */
 import 'vue-awesome/icons'
 
 Vue.config.productionTip = false
-
+Vue.prototype.$http = axios
+// console.log(Vue.prototype)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
