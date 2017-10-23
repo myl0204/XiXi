d852e3cd85fc507f262d73c905bb6ec906b8a416
myl <yunliangmiao@gmail.com>
2017-10-23 23:11:48 +0800

    地点输入组件

---
+++ src/components/mapinput/MapInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="input-wrapper">
+    <div class="time-type">
+      <span class="text">现在</span>
+      <span class="text">预约</span>
+    </div>
+    <div class="input-location">
+      <div class="input-from"></div>
+      <div class="input-to"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.input-wrapper {
+  position: absolute;
+  left: 2%;
+  width: 96%;
+  bottom: 2%;
+}
+</style>
