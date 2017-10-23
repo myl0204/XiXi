2715f95c18abab4f0007736c0ca09f2a8e4fc971
myl <yunliangmiao@gmail.com>
2017-10-23 17:11:55 +0800

    add Vuex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App'
 import axios from 'axios'
 // import router from './router'
@@ -9,11 +10,23 @@ import 'vue-awesome/icons'
 
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
-// console.log(Vue.prototype)
+Vue.use(Vuex)
+/* eslint-disable new-cap */
+const store = new Vuex.Store({
+  state: {
+    curCity: '获取城市中'
+  },
+  mutations: {
+    changeCity(state, city) {
+      state.curCity = city
+    }
+  }
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   // router,
+  store,
   template: '<App/>',
   components: { App }
 })
