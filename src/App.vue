3c647062de5fc0a541e04c279a2010a3b93b1626
myl <yunliangmiao@gmail.com>
2017-11-20 21:54:18 +0800

    引入新增组件

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@
     <v-map></v-map>
     <map-input></map-input>
     <List></List>
+    <User></User>
+    <LogIn></LogIn>
   </div>
 </template>
 
@@ -12,27 +14,33 @@ import Header from '@@/header/Header'
 import vMap from '@@/map/Map'
 import MapInput from '@@/mapinput/MapInput'
 import List from '@@/list/List'
-
+import LogIn from '@@/userlogin/LogIn'
+import User from '@@/user/User'
+import util from '@/common/js/util.js'
 export default {
   name: 'app',
   components: {
     'v-header': Header,
     'v-map': vMap,
     MapInput,
-    List
+    List,
+    User,
+    LogIn
+  },
+  created() {
+    const cookie = util.getCookie()
+    let user = cookie.uid
+    if (user) {
+      this.$store.commit('changeUser', user)
+    }
   },
   beforeMount() {
     let a = document.getElementById('box-wrapper')
     document.body.removeChild(a)
-  },
-  methods: {
-    test() {
-      this.$store.commit('showList', {})
-    }
   }
 }
 </script>
 
 <style lang="scss">
-
+@import './common/scss/base.scss';
 </style>
