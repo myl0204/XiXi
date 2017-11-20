358be4fc96ffde99c0ae05e9b884d55a3c5e5cd3
myl <yunliangmiao@gmail.com>
2017-11-20 21:51:54 +0800

    登录组件的公共api

---
+++ src/common/js/api.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-undef */
+export default {
+  computed: {
+    showFlag() {
+      return this.$store.state.logInStep === this.CURRENT_STEP
+    }
+  },
+  methods: {
+    hide() {
+      this.$store.commit('changeLogInStepTo', this.INIT_STEP)
+    },
+    goPrevStep() {
+      this.$store.commit('changeLogInStepTo', this.PREV_STEP)
+    },
+    goNextStep() {
+      this.$store.commit('changeLogInStepTo', this.NEXT_STEP)
+    }
+  }
+}
