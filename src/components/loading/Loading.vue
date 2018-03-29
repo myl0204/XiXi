86a11ab00d650d177a4fee86c84a87593d1216aa
miao <yunliangmiao@gmail.com>
2018-03-29 23:25:59 +0800

    补上loading组件

---
+++ src/components/loading/Loading.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="dot-wrapper" v-show="loadingShowFlag">
+    <span class="dot dot-1"></span>
+    <span class="dot dot-2"></span>
+    <span class="dot dot-3"></span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    loadingShowFlag: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.dot-wrapper {
+  width: 100%;
+  height: 100%;
+  .dot {
+    display: inline-block;
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    background-color: #fff;
+    &+.dot {
+      margin-left: 5px;
+    }
+    &.dot-1 {
+      animation: scale .5s infinite linear
+    }
+    &.dot-2 {
+      animation: scale .5s .1s infinite linear
+    }
+    &.dot-3 {
+      animation: scale .5s .2s infinite linear
+    }
+  }
+}
+@keyframes scale {
+  0% {
+    transform: scale(1.5)
+  }
+  20% {
+    transform: scale(1)
+  }
+}
+</style>
